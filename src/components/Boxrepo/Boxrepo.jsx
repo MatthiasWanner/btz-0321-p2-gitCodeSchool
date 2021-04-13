464e2ea54b7375b909ce877b9736199ed3d1bf37
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-04-13 11:21:42 +0200

    Add object into array to create Repos List --> Boucle Map to display them

--- src/components/Boxrepo/Boxrepo.jsx
+++ src/components/Boxrepo/Boxrepo.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Boxrepo.css';
 
-function Boxrepo(){
+function Boxrepo({url, userName, repoName, nbStar}){
     return (
         <div className="boxRepo">
-            <img src="" alt=""/>
+            <img src={url} alt={`${userName}image`}/>
             {/* logo */}
-            <h2>userName</h2>
-            <h3>repoName</h3>
-            <p>nbStar★</p>
+            <h2>{userName}</h2>
+            <h3>{repoName}</h3>
+            <p>A récolté {nbStar}★</p>
         </div>
     )
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,11 +4,35 @@ import Form from '../../components/Form/Form'
 import Boxrepo from '../../components/Boxrepo/Boxrepo'
 
 function Home() {
+
+const homeRepos = [
+  { 
+    url : "https://www.editions-delcourt.fr/sites/default/files/styles/image_mis_e/public/2020-06/blagues-de-toto-bloc1-10.jpg?itok=ijOrp6IU",
+    userName : "Beatriz" ,
+    repoName : "TitreB",
+    nbStar : 22 ,
+  },
+
+  { 
+    url : "https://www.editions-delcourt.fr/sites/default/files/styles/image_mis_e/public/2020-06/blagues-de-toto-bloc1-10.jpg?itok=ijOrp6IU",
+    userName : "Polo" ,
+    repoName : "JacksTitle",
+    nbStar : 48,
+  },
+
+  { 
+    url : "https://www.editions-delcourt.fr/sites/default/files/styles/image_mis_e/public/2020-06/blagues-de-toto-bloc1-10.jpg?itok=ijOrp6IU",
+    userName : "Bernardo" ,
+    repoName : "Titre2",
+    nbStar : 2 ,
+  },
+];
+
   return (
     <>
       <div className="baner">
       <h3>Welcome to Git Code School</h3>
-      <img src="#" alt="#"/> 
+      <img src="" alt="image"/> 
       {/* logo */}
       <div className="scrollMore">
         <p>Défile si tu en veux plus</p>
@@ -18,7 +42,10 @@ function Home() {
       </div>
       <div className="backBottom">
         <Form />
-        <Boxrepo />
+        {homeRepos.map((repo , index)=>
+         <Boxrepo key={index} {...repo}  />
+         )}
+        
       </div>
     </>
   );
