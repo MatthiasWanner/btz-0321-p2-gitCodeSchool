1a788f863a94a2d0d2199d1898159f21f1372c0b
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-04-20 15:29:54 +0200

    ‼️ BREAKING: Config

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Dialog } from '@headlessui/react';
+import React from "react";
+import { Dialog } from "@headlessui/react";
 
 function Modal({ children, title, error }) {
   return (
-    <Dialog>
+    <Dialog onClose={() => false} open={true}>
       <Dialog.Overlay className="fixed inset-0" />
     </Dialog>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,29 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.{js,jsx}'
-  ],
+  purge: ['./src/**/*.html', './src/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class',
-  t
+  theme: {
     extend: {
       colors: {
         gold: {
           light: '#CBAC77',
           DEFAULT: '#CBAC77',
           dark: '#CBAC77',
-          hover: '#6C5F47'
+          hover: '#6C5F47',
         },
         homeGray: {
-          dark: '#1D1E1E'
+          dark: '#1D1E1E',
         },
         repos: {
-          dark: '#FCE7D0'
+          dark: '#FCE7D0',
         },
         transparent: 'transparent',
         current: 'currentColor',
         black: colors.black,
-        white: colors.white
-      }
-    }
+        white: colors.white,
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
