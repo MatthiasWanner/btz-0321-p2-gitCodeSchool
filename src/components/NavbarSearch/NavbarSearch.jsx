1aa779dbbc32680355f9296810885d866554dfb0
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-04-22 10:31:20 +0200

    Endpoint is working, trying to get the search

--- src/components/NavbarSearch/NavbarSearch.jsx
+++ src/components/NavbarSearch/NavbarSearch.jsx
@@ -6,28 +6,41 @@ import { useParams } from 'react-router';
 import axios from 'axios';
 
 function NavbarSearch() {
-  const [user, setUser] = useState();
-  const [search, setSearch] = useState();
-  const { username } = useParams();
-  const endpointSearch = PROFIL_URL.replace('{user}', username);
+  const [user, setUser] = useState('');
+  const { username } = useParams('');
+  const [search, setSearch] = useState('');
+  const endpoint = PROFIL_URL.replace('{username}', username);
 
-  console.log(endpointSearch);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUser(user);
+  }
 
   useEffect(() => {
     axios
-      .get(`${API_URL}${endpointSearch}`)
+      .get(`${API_URL}${endpoint}`)
       .then((res) => {
-        console.log(res);
+        setSearch(res);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
 
   return (
     <div className="px-[40px] sm:px-0 flex items-center">
-      <input type="text" placeholder="Search" className="w-full h-6 mr-6 sm:mr-2 pl-3 rounded-full outline-none focus:ring focus:ring-black" />
-      <SearchIcon className="h-8" />
+      <form action="" onClick={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search"
+          value={search}
+          className="w-full h-6 mr-6 sm:mr-2 pl-3 rounded-full outline-none focus:ring focus:ring-black"
+        />
+        <button>
+          <SearchIcon className="h-8" />
+        </button>
+      </form>
     </div>
   );
 }
