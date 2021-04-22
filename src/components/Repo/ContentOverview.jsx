f3e4f3ae31dece0b87cb349b21cb891c2d666c4e
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-04-22 09:50:27 +0200

    Revert "Add something" Ok for commit
    This reverts commit 5b00c6b62405f22cbd91a80629d83350db6246d2.

--- src/components/Repo/ContentOverview.jsx
+++ src/components/Repo/ContentOverview.jsx
@@ -3,28 +3,31 @@ import axios from 'axios';
 import API_URL from '../../api/api';
 import { CONTENT_REPO_URL } from '../../api/endpoints';
 
-export default function ContentOverview({ fileEndPoint }) {
-  const [overviewContent, setOverviewContent] = useState('');
 
-  function b64_to_utf8(str) {
-    return decodeURIComponent(escape(window.atob(str)));
-  }
+export default function ContentOverview({fileEndPoint}){
+    const [overviewContent, setOverviewContent] = useState('')
+    
+    function b64_to_utf8( str ) {
+        return decodeURIComponent(escape(window.atob( str )));
+      }
+    
 
-  useEffect(() => {
-    axios
-      .get(`${API_URL}${fileEndPoint}`)
-      .then((res) => {
-        res.data.content = atob(res.data.content);
-        setOverviewContent(res.data);
-        console.log(res.data);
-      })
-      .catch(console.error);
-  }, [fileEndPoint]);
+    useEffect(()=> {
+        axios
+        .get(`${API_URL}${fileEndPoint}`)
+        .then((res)=>{
+            res.data.content=atob(res.data.content)
+            setOverviewContent(res.data)
+            console.log(res.data)
+        })
+        .catch(console.error)
+    },[fileEndPoint])
 
-  return (
-    <div className="border border-white">
-      <p>{overviewContent.name}</p>
-      <p className="text-white">{overviewContent.content}</p>
-    </div>
-  );
+    return (
+     <div className="border border-white">
+         <p>{overviewContent.name}</p>
+         <p className="text-white" >{overviewContent.content}</p>
+         
+     </div>
+ );
 }
