fb473288b88476a019c35b4fe7ecbdfec381829c
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-05-03 17:41:49 +0200

    📦 NEW: Prune and delete

--- src/components/Chat/ChatBubble.jsx
+++ src/components/Chat/ChatBubble.jsx
@@ -6,6 +6,7 @@ import { ChatContext } from '../Contexts';
 import Tooltip from '../Tooltip/Tooltip';
 
 import './ChatBubble.css';
+import { TrashIcon } from '@heroicons/react/solid';
 
 function ChatBubble({ username, recipient }) {
   const socket = useContext(ChatContext);
@@ -23,9 +24,21 @@ function ChatBubble({ username, recipient }) {
       setMessages(newMessages);
     });
 
+    socket.on('chat:delete', () => {
+      console.log('Chat deleted');
+      setMessages([]);
+    });
+
+    socket.on('message:delete', (deletedMessages) => {
+      console.log('message deleted', deletedMessages);
+      setMessages(deletedMessages);
+    });
+
     return () => {
       socket.off('chat:open');
+      socket.off('chat:delete');
       socket.off('message:create');
+      socket.off('message:delete');
     };
   }, [recipient]);
 
@@ -52,21 +65,34 @@ function ChatBubble({ username, recipient }) {
     setContent('');
   };
 
+  const handlePrune = () => {
+    socket.emit('chat:delete', recipient);
+  };
+
   return (
     <div>
+      <button className="pl-1 pr-3 flex hover:bg-gold-dark focus:outline-none focus:bg-gold-dark rounded-full" onClick={handlePrune}>
+        <TrashIcon className="h-12 md:h-6" />
+        <p className="hidden md:block">Effacer la discussion</p>
+      </button>
       Messages a {recipient}:
       <br />
-      {messages.map((msg) => (
-        <Fragment key={msg.date}>
-          {
-            <div className={`flex ${msg.from === username ? 'flex-row-reverse' : 'flex-row'} w-full md:w-1/2 py-2`}>
-              <p className={msg.from === username ? 'ml-1' : 'mr-1'}>{msg.from}</p>
-              <div className="has-tooltip bg-gold-dark rounded-full px-3 w-full">
-                {msg.content} <Tooltip>{formatDate(msg.date)}</Tooltip>
-              </div>
-            </div>
-          }
-        </Fragment>
+      {messages.map((msg, index) => (
+        <div key={msg.date} className={`flex ${msg.from === username ? 'flex-row-reverse' : 'flex-row'} w-full md:w-1/2 py-2`}>
+          <p className={msg.from === username ? 'ml-1' : 'mr-1'}>{msg.from}</p>
+          <div className="has-tooltip bg-gold-dark rounded-full px-3 w-full">
+            <p>{msg.content}</p>
+            <Tooltip>{formatDate(msg.date)}</Tooltip>
+          </div>
+          {msg.from === username && (
+            <button
+              onClick={() => {
+                socket.emit('message:delete', recipient, index);
+              }}>
+              <TrashIcon className="h-full" />
+            </button>
+          )}
+        </div>
       ))}
       <form action="" onSubmit={handleSubmit}>
         <input
@@ -82,6 +108,7 @@ function ChatBubble({ username, recipient }) {
 }
 
 ChatBubble.propTypes = {
+  username: PropTypes.string,
   recipient: PropTypes.string,
 };
 
