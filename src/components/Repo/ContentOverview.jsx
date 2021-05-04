6d00c5b2947cefec16bac7fda90d2502600d5963
rcarondw <r.carondw@gmail.com>
2021-05-04 18:05:59 +0200

    petit changement de css pas prévus avant changement de branch

--- src/components/Repo/ContentOverview.jsx
+++ src/components/Repo/ContentOverview.jsx
@@ -8,9 +8,9 @@ export default function ContentOverview({ fileEndPoint }) {
   const overviewContent = useGetFile(fileEndPoint);
 
   return (
-    <div className="content2 border border-gold-dark  mb-6 px-6 py-4 rounded-md bg-homeGray-dark">
-      <ReactMarkdown className="text-white flex justify-center pb-3 border-b border-gold-dark">{overviewContent.datas.name}</ReactMarkdown>
-      <ReactMarkdown className="text-white px-2 break-words pt-3 text-justify">{overviewContent.datas.content}</ReactMarkdown>
+    <div className="content2 border border-gold-dark  mb-6 px-6 py-4 rounded-md bg-homeGray-dark overflow-auto">
+      <ReactMarkdown className="text-white flex justify-center pb-3 border-b border-gold-dark ">{overviewContent.datas.name}</ReactMarkdown>
+      <ReactMarkdown className="text-white px-2 break-words pt-3 text-justify overflow-scroll">{overviewContent.datas.content}</ReactMarkdown>
     </div>
   );
 }
