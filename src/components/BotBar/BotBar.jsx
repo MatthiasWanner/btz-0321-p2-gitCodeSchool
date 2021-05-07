520b23569bbcea093e01a9f8bdeb8c5d29be83ed
rcarondw <r.carondw@gmail.com>
2021-05-07 13:39:38 +0200

    change color usersicon botbar

--- src/components/BotBar/BotBar.jsx
+++ src/components/BotBar/BotBar.jsx
@@ -24,7 +24,7 @@ function BotBar() {
       <div className="md:flex md:w-full md:mb-16 md:items-end md:justify-end">
         <div className="md:w-16 md:h-16 md:rounded-full md:border md: md:bottom-0 h-16 w-full flex justify-center items-center border-t border-gold-dark bg-homeGray-darker  fixed bottom-0 visible z-50 mb-2 mr-2">
           <button onClick={() => setChat(!chat)}>
-            <UsersIcon className="h-8 text-white flex focus:outline-none" />
+            <UsersIcon className="h-8 text-gold-dark flex focus:outline-none" />
           </button>
           {unreadMessage > 0 && <div className="absolute self-start h-4 w-4 bg-red-800 animate-pulse rounded-full" />}
         </div>
