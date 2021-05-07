a0eeef7350323e78e15666a8f4859beacd0cda8f
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-05-07 10:17:46 +0200

    Update Contexts.js

--- src/components/Contexts.js
+++ src/components/Contexts.js
@@ -2,5 +2,5 @@ import { createContext } from 'react';
 import io from 'socket.io-client';
 
 export const ModalContext = createContext(null);
-export const ChatContext = createContext(io('http://localhost:8080'));
+export const ChatContext = createContext(io('https://websocket-server-gitcodeschool.herokuapp.com/'));
 export const UnreadMessageContext = createContext();
